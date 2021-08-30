4c88a398777a7133272f61b77a7aa4ceaa00ce3a
MIguel808 <miguel.soto.t@tecsup.edu.pe>
2021-08-29 23:55:47 -0500

    propuesto1

--- lab02/lab02/urls.py
+++ lab02/lab02/urls.py
@@ -2,9 +2,13 @@ from django import urls
 from lab02 import encuesta
 from django.contrib import admin
 from django.urls import include, path
+from CALCULADORA.views import suma, resta, multiplicar
 
 urlpatterns = [
     path('encuesta/', include(encuesta.urls)),
     path('admin/', admin.site.urls),
+    path('app/suma/<int:calculo_id>/<int:calculo2_id>',suma),
+    path('app/resta/<int:calculo_id>/<int:calculo2_id>',resta),
+    path('app/multiplica/<int:calculo_id>/<int:calculo2_id>',multiplicar),
 ]
 
